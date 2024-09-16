5b599f351450d3445fd38dd8872f9b9050a18d87
Polina Novikova <novpolina99@mail.ru>
2024-09-16 19:44:32 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+
+
+int main(){
+
+    std::cout << "Hello, World!" << std::endl;
+    int a = 9;
+    int b = 10;
+    int c = a+b;
+    printf("ytllj %d", c);
+    return 0; 
+
+}
